e728216f418af2c39b7d29bc8bd18c46da7b3fc6
Luiz H. S. Mileck <luizhmileck@gmail.com>
2020-10-27 13:20:09 -0300

    first commit

---
+++ musicians/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Musician(models.Model):
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    instrumento = models.CharField(max_length=255)
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    musician = models.ForeignKey(Musician, on_delete=models.CASCADE)
+
+# Create your models here.
